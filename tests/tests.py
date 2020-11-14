d8eabec1cd920cb64a9fffce2c46e73159257356
Alexander Leonhardt <equinox.salexander@gmail.com>
2020-11-14 19:20:16 +0100

    Added a basic unit test file for later use

---
+++ tests/tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestWikipediaCmd(unittest.TestCase):
+    """Will unit test the wikipedia command module as soon as there is something
+    to test"""
+    def test_upper(self):
+        """Dummy test just to have it running"""
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
+
